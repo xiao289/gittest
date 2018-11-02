eb402800aa6f09ed353592f0617e9463e6cfb33a
xiao289 <2412092531@qq.com>
2018-11-02 11:19:20 +0800

    aaa

---
+++ ssm1016/src/main/java/com/service/impl/UserTbServiceImpl.java
@@ -0,0 +1,71 @@
+package com.service.impl;
+
+import com.bean.UserTb;
+import com.dao.UserTbMapper;
+import com.service.UserTbService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Service
+public class UserTbServiceImpl implements UserTbService {
+
+    @Autowired
+    private UserTbMapper userTbMapper;
+
+    @Override
+    public int deleteByPrimaryKey(Integer userId) {
+        return 0;
+    }
+
+    @Override
+    public int insert(UserTb record) {
+        return 0;
+    }
+
+    @Override
+    public int insertSelective(UserTb record) {
+        return 0;
+    }
+
+    @Override
+    public UserTb selectByPrimaryKey(Integer userId) {
+        return userTbMapper.selectByPrimaryKey(userId);
+    }
+
+    @Override
+    @Transactional
+    public int updateByPrimaryKeySelective(UserTb record) {
+        return userTbMapper.updateByPrimaryKeySelective(record);
+    }
+
+    @Override
+    public int updateByPrimaryKey(UserTb record) {
+        return 0;
+    }
+
+    @Override
+    public UserTb login(UserTb userTb) {
+        UserTb userTb1=  userTbMapper.login(userTb.getUserName());
+        if(userTb1!=null&&userTb1.getUserPs().equals(userTb.getUserPs())){
+            //修改登录次数
+              userTb1.setLogincount(userTb1.getLogincount()+1);
+              userTbMapper.updateByPrimaryKeySelective(userTb1);
+            return userTb1;
+        }
+        return null;
+    }
+
+    @Override
+    public List findall(int did, int mid, String rolename) {
+        Map map=new HashMap();
+        map.put("did",did);
+        map.put("mid",mid);
+        map.put("rolename",rolename);
+        return userTbMapper.findall(map);
+    }
+}
